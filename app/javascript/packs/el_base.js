bc3447bddc3f51e79fce7c741b07d3c60b7369c5
yusaku <>
2018-11-26 23:24:26 +0900

    add images & more

---
+++ app/javascript/packs/el_base.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import element from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+
+Vue.use(element, {locale})
+
+new Vue({
+  el: '#el_base',
+  data: {},
+  methods: {}
+})
--- app/javascript/packs/index2.js
+++
@@ -1,4 +0,0 @@
-import '@babel/polyfill';
-
-let aaa = '22222222'
-console.log(`bbb${aaa}ccc`)
